05ae658e30024e2d1d593c115a98cf3104a14f7a
André Carrilho <andre-luiz-carrilho-de-aguiar@impulso.network>
2020-05-09 19:12:52 -0300

    Including chi go api

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/99designs/gqlgen v0.11.3
-	github.com/olivere/elastic/v7 v7.0.15 
+	github.com/go-chi/chi v4.1.1+incompatible // indirect
+	github.com/olivere/elastic/v7 v7.0.15
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server.go
+++ server.go
@@ -6,14 +6,15 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/carrilhoandre/webmotors-search-go/graph"
 	"github.com/carrilhoandre/webmotors-search-go/graph/generated"
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	r := chi.NewRouter()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -21,9 +22,9 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	r.Handle("/query", srv)
+	//http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	//http.Handle("/query", srv)
+	log.Println("server is running on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
