df3939569f254b21a8500dc25cd6d054a2c2563b
André Carrilho <andre-luiz-carrilho-de-aguiar@impulso.network>
2020-05-09 21:36:19 -0300

    .

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/carrilhoandre/webmotors-search-go/graph"
@@ -15,16 +14,10 @@ const defaultPort = "8080"
 
 func main() {
 	r := chi.NewRouter()
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	go r.Handle("/query", srv)
 	//http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	//http.Handle("/query", srv)
 	log.Println("server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
